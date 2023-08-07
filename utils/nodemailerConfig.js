c8e0e8406a3aa461c6dae1147d721630676c94f3
Ihlassul2023 <ihlascamp@gmail.com>
2023-08-07 13:58:13 +0800

    update

--- utils/nodemailerConfig.js
+++ utils/nodemailerConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: "smtp.ethereal.email",
-  port: 587,
+  host: process.env.HOST_NODEMAILER,
+  port: process.env.PORT_NODEMAILER,
   auth: {
-    user: "adelia40@ethereal.email",
-    pass: "fWhmfdhjJhNMR5qev1",
+    user: process.env.USER_NODEMAILER,
+    pass: process.env.PASS_NODEMAILER,
   },
 };
