095819e822f6dade7f7e6d614c33a3fec226fd87
Ihlassul2023 <ihlascamp@gmail.com>
2023-09-09 12:00:52 +0800

    update routes

--- routes/likeSavedRoute.js
+++ routes/likeSavedRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { postLike, getLikedRecipe, postSave, getSavedRecipe } = require("../controllers/likeSaved");
 const { authenticateUser } = require("../middlewares/authentication");
 router.route("/like").get(authenticateUser, getLikedRecipe);
-router.route("/bookmark").get(authenticateUser, getSavedRecipe).post(authenticateUser, postSave);
+router.route("/bookmark").get(authenticateUser, getSavedRecipe);
 router.post("/like/:recipe_id", authenticateUser, postLike);
 router.post("/bookmark/:recipe_id", authenticateUser, postSave);
 
