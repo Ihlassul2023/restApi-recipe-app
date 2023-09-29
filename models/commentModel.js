54784ce1fc90d9bb7651ea1bae7d647ff64f4ed4
Ihlassul2023 <ihlascamp@gmail.com>
2023-09-29 13:24:49 +0800

    fix error comment model

--- models/commentModel.js
+++ models/commentModel.js
@@ -14,7 +14,7 @@ const postCommentQuery = async (data) => {
 
 const getCommentRecipeQuery = async (id) => {
   return new Promise((resolve, reject) =>
-    Pool.query(`SELECT user.name, user.photo_user, comment.text FROM comment JOIN user ON comment.user_id=user.id WHERE comment.recipe_id=${id} `, (err, result) => {
+    Pool.query(`SELECT users.name, users.photo_user, comment.text FROM comment JOIN users ON comment.user_id=users.id WHERE comment.recipe_id=${id} `, (err, result) => {
       if (!err) {
         resolve(result);
       } else {
