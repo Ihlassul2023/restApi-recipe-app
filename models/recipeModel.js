5a42d10d0eeaadd6651fa7dee85e4f4f7eb42db0
Ihlassul2023 <ihlascamp@gmail.com>
2023-10-04 14:38:47 +0800

    update model

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -5,7 +5,7 @@ const getRecipeAllQuery = async (data) => {
   const { search, searchBy, offset, limit, sort } = data;
   return new Promise((resolve, reject) =>
     Pool.query(
-      `SELECT recipe.id, recipe.title, recipe.ingredients, recipe.photo, recipe.like_count,recipe.saved_count,recipe.comment_count,recipe.created_at AS created, users.photo_user AS profil_pict, category.name AS category,users.name AS author FROM recipe JOIN category ON recipe.category_id = category.id JOIN users ON recipe.user_id = users.id  WHERE ${searchBy} ILIKE '%${search}%' ORDER BY recipe.like_count ${sort} OFFSET ${offset} LIMIT ${limit}`,
+      `SELECT recipe.id, recipe.title, recipe.ingredients, recipe.user_id, recipe.photo, recipe.like_count,recipe.saved_count,recipe.comment_count,recipe.created_at AS created, users.photo_user AS profil_pict, category.name AS category,users.name AS author FROM recipe JOIN category ON recipe.category_id = category.id JOIN users ON recipe.user_id = users.id  WHERE ${searchBy} ILIKE '%${search}%' ORDER BY recipe.like_count ${sort} OFFSET ${offset} LIMIT ${limit}`,
       (err, result) => {
         if (!err) {
           resolve(result);
@@ -21,7 +21,7 @@ const getMyRecipeQuery = async (data) => {
   const { search, searchBy, offset, limit, id, sort } = data;
   return new Promise((resolve, reject) =>
     Pool.query(
-      `SELECT recipe.id,recipe.category_id, recipe.title, recipe.ingredients,recipe.like_count,recipe.saved_count,recipe.comment_count, recipe.photo,recipe.public_id, category.name AS category,users.name AS author FROM recipe JOIN category ON recipe.category_id = category.id JOIN users ON recipe.user_id = users.id WHERE user_id = ${id} AND ${searchBy} ILIKE '%${search}%' OFFSET ${offset} LIMIT ${limit}`,
+      `SELECT recipe.id,recipe.category_id, recipe.title, recipe.user_id, recipe.ingredients,recipe.like_count,recipe.saved_count,recipe.comment_count, recipe.photo,recipe.public_id, category.name AS category,users.name AS author FROM recipe JOIN category ON recipe.category_id = category.id JOIN users ON recipe.user_id = users.id WHERE user_id = ${id} AND ${searchBy} ILIKE '%${search}%' OFFSET ${offset} LIMIT ${limit}`,
       (err, result) => {
         if (!err) {
           resolve(result);
@@ -88,7 +88,7 @@ const putRecipeQuery = async (data) => {
 const getRecipeByIdQuery = async (id) => {
   return new Promise((resolve, reject) =>
     Pool.query(
-      `SELECT recipe.id,recipe.category_id, recipe.title,recipe.created_at, recipe.ingredients,recipe.like_count,recipe.saved_count,recipe.comment_count, recipe.photo,recipe.public_id, category.name AS category,users.name AS author, users.photo_user FROM recipe JOIN category ON recipe.category_id = category.id JOIN users ON recipe.user_id = users.id  WHERE recipe.id=${id}`,
+      `SELECT recipe.id,recipe.category_id,recipe.user_id, recipe.title,recipe.created_at, recipe.ingredients,recipe.like_count,recipe.saved_count,recipe.comment_count, recipe.photo,recipe.public_id, category.name AS category,users.name AS author, users.photo_user FROM recipe JOIN category ON recipe.category_id = category.id JOIN users ON recipe.user_id = users.id  WHERE recipe.id=${id}`,
       (err, result) => {
         if (!err) {
           resolve(result);
